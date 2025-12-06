7d824c7dc54985ef55421d5dbccf3b2e0fd64462
Owen <owen.debiasio@gmail.com>
2025-12-05 21:19:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo -e "bloled will now install.\nPress enter to continue...\n"
 read
 
 echo "Copying files..."
+rm -rf $HOME/.local/share/zed/extensions/installed/bloled/ | echo "Command isn't already installed, skipping"
 cp -r bloled $HOME/.local/share/zed/extensions/installed/
 
 echo "Done!"
