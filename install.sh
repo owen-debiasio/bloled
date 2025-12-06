e2d077055d5902a57f371e574a5e396ab116b387
Owen <owen.debiasio@gmail.com>
2025-12-05 21:21:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,20 @@ clear
 echo -e "bloled will now install.\nPress enter to continue...\n"
 read
 
+DEST="$HOME/.local/share/zed/extensions/installed/bloled"
+
 echo "Copying files..."
-rm -rf $HOME/.local/share/zed/extensions/installed/bloled/ | echo "Command isn't already installed, skipping"
-cp -r bloled $HOME/.local/share/zed/extensions/installed/
+
+if [ -d "$DEST" ]; then
+    rm -rf "$DEST"
+    echo "Removed old version."
+else
+    echo "No previous install found, skipping removal."
+fi
+mkdir -p "$HOME/.local/share/zed/extensions/installed/"
+
+# Copy the folder next to the script
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cp -r "$SCRIPT_DIR/bloled" "$HOME/.local/share/zed/extensions/installed/"
 
 echo "Done!"
