53314915e243ec24a95089bbf97db10d74ebe131
Owen <owen.debiasio@gmail.com>
2025-12-05 21:31:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 clear
 echo -e "bloled will now install.\nPress Enter to continue...\n"
 
-# Make the pause work even with curl | bash
 if [ -t 0 ]; then
     read -r
 else
@@ -20,18 +19,14 @@ git clone --depth=1 "$REPO_URL" "$TMP_DIR"
 
 echo "Copying files..."
 
-# Remove old install
 if [ -d "$DEST" ]; then
     rm -rf "$DEST"
     echo "Removed previous install."
 else
-    echo "Command isn't already installed, skipping"
+    echo "Theme isn't already installed, skipping"
 fi
 
-# Ensure parent dir exists
 mkdir -p "$HOME/.local/share/zed/extensions/installed/"
-
-# Copy the actual extension folder from the repo
 cp -r "$TMP_DIR/bloled" "$DEST"
 
 echo "Cleaning up..."
